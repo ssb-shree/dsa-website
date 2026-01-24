5db069b3872dcdadde19699c7fc44cc1c8710fc7
ssb-shree <bavachikarshree1666@gmail.com>
2026-01-24 11:44:03 +0530

    add motion to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,19 @@
+"use client";
 import Link from "next/link";
-
-import { Kelly_Slab } from "next/font/google";
-
-const kelly = Kelly_Slab({
-  weight: "400",
-  subsets: ["latin"],
-});
+import { motion } from "framer-motion";
 
 const Footer = () => {
   return (
     <>
       <footer className="w-screen h-[40vh] p-2 overflow-x-hidden flex flex-col md:flex-row gap-y-3 justify-around items-center">
         <div className="w-full md:w-[35%] flex flex-col justify-center items-start">
-          <span className="text-8xl md:text-9xl text-red-600 tracking-widest">DSA</span>
+          <span className="text-8xl md:text-9xl text-red-600 tracking-widest flex flex-row justify-center items-center space-x-2">
+            {"DSA".split("").map((letter, index) => (
+              <motion.span key={index} whileHover={{ y: -2, scale: 1.2 }} className="inline-block hover:cursor-pointer">
+                {letter}
+              </motion.span>
+            ))}
+          </span>
           <p className="text-sm md:text-md">Representing the finest of Data Science department.</p>
         </div>
         <div className="w-full md:w-[40%] flex flex-row justify-around items-center">
