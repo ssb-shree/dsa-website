247ec0dcd07708bdcda41e52975c508a256a5ef0
ssb-shree <bavachikarshree1666@gmail.com>
2026-01-24 18:12:34 +0530

    add social media links to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,15 @@
 import Link from "next/link";
 import { motion } from "framer-motion";
 
+const socialLinks: { name: string; link: string }[] = [
+  { name: "instagram", link: "https://www.instagram.com/dsa_apsit" },
+  {
+    name: "linkedin",
+    link: "https://www.linkedin.com/in/dsa-apsit?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+  },
+  { name: "facebook", link: "https://www.facebook.com/p/Data-Science-Association-APSIT-100085633250918/" },
+];
+
 const Footer = () => {
   return (
     <>
@@ -17,11 +26,11 @@ const Footer = () => {
           <p className="text-sm md:text-md">Representing the finest of Data Science department.</p>
         </div>
         <div className="w-full md:w-[40%] flex flex-row justify-around items-center">
-          <div className="flex flex-col justify-center items-center gap-y-3">
-            <span>list of links 1</span>
-            {[1, 2, 3, 4].map((link, index) => (
-              <Link href={"#"} key={index} className="underline text-md">
-                link {link}
+          <div className="flex flex-col justify-center items-start gap-y-3">
+            <span>Follow Us</span>
+            {socialLinks.map((item, index) => (
+              <Link href={item.link} target="_blank" key={index} className="underline text-md capitalize text-left">
+                {item.name}
               </Link>
             ))}
           </div>
@@ -35,8 +44,12 @@ const Footer = () => {
           </div>
         </div>
       </footer>
-      <Link href={"#"} className="w-screen flex justify-center items-center mb-5">
-        yours truly
+      <Link
+        href="https://www.linkedin.com/in/shree-bavachikar-a16493375/"
+        target="_blank"
+        className="w-screen flex justify-center items-center mb-5"
+      >
+        Built by Shree
       </Link>
     </>
   );
