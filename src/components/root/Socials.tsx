09d50cdfa1652e14e325e5f4ae3be74387df9516
ssb-shree <bavachikarshree1666@gmail.com>
2026-01-24 10:55:37 +0530

    minor ui updates

---
+++ src/components/root/Socials.tsx
@@ -0,0 +1,7 @@
+const Socials = () => {
+  return (
+    <section className="w-screen h-screen overflow-hidden bg-zinc-950 flex justify-center items-center">Social</section>
+  );
+};
+
+export default Socials;
---
+++ src/components/root/Team.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Team = () => {
+  return <section className="w-screen h-screen teams overflow-hidden flex justify-center items-center">Team</section>;
+};
+
+export default Team;
