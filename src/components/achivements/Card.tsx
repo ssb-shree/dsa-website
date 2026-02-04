90f14a28e20a67419bdeab3797fe520a04b12a12
ssb-shree <bavachikarshree1666@gmail.com>
2026-02-05 00:38:23 +0530

    add prop types

--- src/components/achivements/Card.tsx
+++ src/components/achivements/Card.tsx
@@ -1,4 +1,4 @@
-export default function Card({ imgUrl, alt = "" }: Card) {
+export default function Card({ imgUrl, alt = "" }: { imgUrl: string; alt: string }) {
   return (
     <div className=" w-full h-[220px] md:h-[360px] bg-black overflow-hidden rounded-md overflow-hidden">
       <img src={imgUrl} alt={alt} className="h-full w-full object-cover" loading="lazy" />
