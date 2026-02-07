389d1d8620eebcffc7230b1639480c8d2417b784
ssb-shree <bavachikarshree1666@gmail.com>
2026-02-07 23:53:52 +0530

    schema update

--- server/src/controllers/achievement.schema.ts
+++ server/src/controllers/achievement.schema.ts
@@ -1,13 +1,13 @@
 import z from "zod";
 
 const achievementSchema = z.object({
-  name: z.string().min(1),
+  title: z.string().min(1),
   description: z.string().min(1),
   imgUrl: z.string().url(),
 });
 
 const editAchievementSchema = z.object({
-  name: z.string().min(1).optional(),
+  title: z.string().min(1).optional(),
   description: z.string().min(1).optional(),
   imgUrl: z.string().url().optional(),
 });
