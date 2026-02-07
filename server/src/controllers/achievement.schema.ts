a452ade6436148be06573a95035cf23e46bb0347
ssb-shree <bavachikarshree1666@gmail.com>
2026-02-07 23:58:12 +0530

    schema update

--- server/src/controllers/achievement.schema.ts
+++ server/src/controllers/achievement.schema.ts
@@ -4,12 +4,14 @@ const achievementSchema = z.object({
   title: z.string().min(1),
   description: z.string().min(1),
   imgUrl: z.string().url(),
+  date: z.string().min(1),
 });
 
 const editAchievementSchema = z.object({
   title: z.string().min(1).optional(),
   description: z.string().min(1).optional(),
   imgUrl: z.string().url().optional(),
+  date: z.string().min(1).optional(),
 });
 
 export { editAchievementSchema, achievementSchema };
