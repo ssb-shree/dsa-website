f101544ce665467e671ec0d465695a79081026cf
ssb-shree <bavachikarshree1666@gmail.com>
2026-02-07 21:22:17 +0530

    removed typo files

--- client/src/components/achivements/Card.tsx
+++
@@ -1,7 +0,0 @@
-export default function Card({ imgUrl, alt = "" }: { imgUrl: string; alt: string }) {
-  return (
-    <div className=" w-full h-[220px] md:h-[360px] bg-black overflow-hidden rounded-md overflow-hidden">
-      <img src={imgUrl} alt={alt} className="h-full w-full object-cover" loading="lazy" />
-    </div>
-  );
-}
