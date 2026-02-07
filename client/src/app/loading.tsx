c5fa0fc49512b8a10353ed76b3baec36dfdf8b8d
ssb-shree <bavachikarshree1666@gmail.com>
2026-02-07 18:02:00 +0530

    add loading page

---
+++ client/src/app/loading.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { CgSpinnerAlt } from "react-icons/cg";
+
+const LoadingPage = () => {
+  const message = "Still loading. Still iconic.";
+
+  return (
+    <section className="w-screen h-[60vh] flex flex-col justify-center items-center">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.3 }}
+        className="flex flex-col items-center space-y-6"
+      >
+        {/* Spinner */}
+        <CgSpinnerAlt size={30} className="animate-spin" />
+
+        {/* Text */}
+        <motion.p
+          initial={{ y: 10, opacity: 0 }}
+          animate={{ y: 0, opacity: 0.8 }}
+          transition={{ delay: 0.2, duration: 0.3, ease: [0.16, 1, 0.3, 1] }}
+          className="text-sm text-center max-w-xs"
+        >
+          {message}
+        </motion.p>
+      </motion.div>
+    </section>
+  );
+};
+
+export default LoadingPage;
