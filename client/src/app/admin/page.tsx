8dc3c1384395113a2fa39fa7c3c43725a0517e22
ssb-shree <bavachikarshree1666@gmail.com>
2026-02-07 22:44:26 +0530

    dropped for now, todo cms admin page later

--- client/src/app/admin/page.tsx
+++ client/src/app/admin/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+"use client";
+
+import AchievementSection from "@/components/admin/AchievementSection";
+import EventSection from "@/components/admin/EventSection";
+import TeamSection from "@/components/admin/TeamSection";
 
 const AdminPage = () => {
   return (
-    <div>AdminPage</div>
-  )
-}
+    <section className="w-screen min-h-screen bg-zinc-200 text-black p-8 flex flex-col items-center justify-center gap-12">
+      {/* Team Section */}
+      <div className="w-full max-w-5xl mx-auto bg-white rounded-lg p-6 shadow-md">
+        <TeamSection />
+      </div>
 
-export default AdminPage
+      {/* Event Section */}
+      <div className="w-full max-w-5xl mx-auto bg-white rounded-lg p-6 shadow-md">
+        <EventSection />
+      </div>
+
+      {/* Achievement Section */}
+      <div className="w-full max-w-5xl mx-auto bg-white rounded-lg p-6 shadow-md">
+        <AchievementSection />
+      </div>
+    </section>
+  );
+};
+
+export default AdminPage;
