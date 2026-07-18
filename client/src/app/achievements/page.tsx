6fa33bb0503b4c1f6ceed0f17b0d2bc613408ad2
ssb-shree <bavachikarshree1666@gmail.com>
2026-07-18 17:26:14 +0530

    Add try catch block to async component

--- client/src/app/achievements/page.tsx
+++ client/src/app/achievements/page.tsx
@@ -6,30 +6,32 @@ import axiosInstance from "@/services/axios";
 import LoadingPage from "../loading";
 
 const AchievementsPage = async () => {
-  const { data } = await axiosInstance.get(`/achievements`);
+  try {
+    const { data } = await axiosInstance.get(`/achievements`);
 
-  console.log(data)
+    if (data.achievements.length === 0) {
+      return <LoadingPage />;
+    }
 
-  if (data.achievements.length === 0) {
+    const content = data.achievements.map((item: any) => ({
+      name: item.title,
+      description: item.description,
+      content: <Card imgUrl={item.imgUrl} alt={item.title} />,
+    }));
+
+    return (
+      <section className="h-screen w-screen bg-zinc-950">
+        <div className="hidden md:flex w-full h-full justify-center items-center">
+          <Desktop content={content} />
+        </div>
+        <div className="flex md:hidden w-full h-full justify-center items-center">
+          <Mobile content={content} />
+        </div>
+      </section>
+    );
+  } catch (error) {
     return <LoadingPage />;
   }
-
-  const content = data.achievements.map((item: any) => ({
-    name: item.title,
-    description: item.description,
-    content: <Card imgUrl={item.imgUrl} alt={item.title} />,
-  }));
-
-  return (
-    <section className="h-screen w-screen bg-zinc-950">
-      <div className="hidden md:flex w-full h-full justify-center items-center">
-        <Desktop content={content} />
-      </div>
-      <div className="flex md:hidden w-full h-full justify-center items-center">
-        <Mobile content={content} />
-      </div>
-    </section>
-  );
 };
 
 export default AchievementsPage;
